584cd5c7a2b0e43f734cac5121503aee70a22441
Drew Buckman <dbuckma@us.ibm.com>
2017-01-17 13:49:16 -0500

    tested in bluemix

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,11 @@ from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 app.debug = False
-app.debug = True
-app.config.from_object('config')
+#app.debug = True
 
-if (app.debug):
-  from werkzeug.debug import DebuggedApplication
-  app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
+#if (app.debug):
+  #from werkzeug.debug import DebuggedApplication
+  #app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
 
 from pages.views import pageView as pageView
 app.register_blueprint(pageView)
